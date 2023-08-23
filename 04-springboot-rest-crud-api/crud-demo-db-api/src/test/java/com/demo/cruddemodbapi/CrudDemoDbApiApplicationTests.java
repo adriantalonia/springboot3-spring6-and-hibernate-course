a1a78599c58f64591f161581bc2b361e8c6f9131
Adrian <adrian.talonia11@gmail.com>
2023-08-22 23:17:35 -0600

    init crud demo db

---
+++ 04-springboot-rest-crud-api/crud-demo-db-api/src/test/java/com/demo/cruddemodbapi/CrudDemoDbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.cruddemodbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoDbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
